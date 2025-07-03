9e4041bf64ee2a3a3602f2b5f1d39c84b2dd2675
MyintMyatThukha <myintmyatthukha734@gmail.com>
2025-07-03 09:13:15 +0630

    Add files via upload

---
+++ InterfaceOne.java
@@ -0,0 +1,11 @@
+package exampleDHHM;
+
+public interface InterfaceOne {
+	// abstract methods
+	public abstract void show();
+
+	public abstract int sum(int num1, int num2);
+
+	public abstract int sub(int num1, int num2);
+
+}
